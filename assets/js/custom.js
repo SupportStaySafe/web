06b0cec13426f8d173ba12a9135b0cfb3d624df8
eka-ekreasi <eka@ekreasi.com>
2020-05-26 07:55:16 +0700

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -40,6 +40,18 @@ function init(){
     langue(indonesia,english);
 }
 init();
+
+var myVar;
+
+function myFunction() {
+myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+document.getElementById("loader").style.display = "none";
+document.getElementById("myDiv").style.display = "block";
+}
+
 $('.navbar-collapse a').click(function(){
     $(".navbar-collapse").collapse('hide');
 });
